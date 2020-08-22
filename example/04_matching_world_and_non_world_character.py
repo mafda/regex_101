a1bf236fc8dc14399023b467722b2edc3f921723
fernanda rodríguez <mafda@users.noreply.github.com>
2020-08-22 14:14:07 -0300

    Create 04_matching_world_and_non_world_character.py

---
+++ example/04_matching_world_and_non_world_character.py
@@ -0,0 +1,11 @@
+``` 
+    task
+    You have a test string S. Your task is to match the pattern xxxXxxxxxxxxxxXxxx
+    Here x denotes any word character and X denotes any non-word character.
+```
+
+import re
+
+Regex_Pattern = r"[\w]{3}\W[\w]{10}\W[\w]{3}"
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
