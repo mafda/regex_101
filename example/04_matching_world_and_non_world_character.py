8a352f3ccfbe67b6e4352a1f40b1ab711a3331f9
fernanda rodríguez <mafda@users.noreply.github.com>
2020-08-23 19:35:16 -0300

    Update 04_matching_world_and_non_world_character.py

--- example/04_matching_world_and_non_world_character.py
+++ example/04_matching_world_and_non_world_character.py
@@ -1,8 +1,8 @@
-``` 
+"""
     task
     You have a test string S. Your task is to match the pattern xxxXxxxxxxxxxxXxxx
     Here x denotes any word character and X denotes any non-word character.
-```
+"""
 
 import re
 
