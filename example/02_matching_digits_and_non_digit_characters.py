04f04859b1026ca6c91cede75513479d9fcef832
fernanda rodríguez <mafda@users.noreply.github.com>
2020-08-29 12:23:25 -0300

    Update 02_matching_digits_and_non_digit_characters.py

--- example/02_matching_digits_and_non_digit_characters.py
+++ example/02_matching_digits_and_non_digit_characters.py
@@ -1,3 +1,9 @@
+"""
+  Task
+  You have a test string S. Your task is to match the pattern xxXxxXxxxx
+  Here x denotes a digit character, and X denotes a non-digit character.
+"""
+
 import re
 
 Regex_Pattern = r"\d\d\D\d\d\D\d\d\d\d"
