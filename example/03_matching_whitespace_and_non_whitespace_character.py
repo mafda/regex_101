259e3ebae8c2c1f5bd82ac273e152e4a78a43a94
fernanda rodríguez <mafda@users.noreply.github.com>
2020-08-29 12:24:36 -0300

    Update 03_matching_whitespace_and_non_whitespace_character.py

--- example/03_matching_whitespace_and_non_whitespace_character.py
+++ example/03_matching_whitespace_and_non_whitespace_character.py
@@ -1,3 +1,9 @@
+"""
+  Task
+  You have a test string S. Your task is to match the pattern XXxXXxXX
+  Here, x denotes whitespace characters, and X denotes non-white space characters.
+"""
+
 import re
 
 Regex_Pattern = r"\S\S\s\S\S\s\S\S"
