b73a5ebad49bc10508351ef180cd90884300ded0
fernanda rodríguez <mafda@users.noreply.github.com>
2020-08-29 12:22:20 -0300

    Update 01_matching_anything_but_a_newline.py

--- example/01_matching_anything_but_a_newline.py
+++ example/01_matching_anything_but_a_newline.py
@@ -1,3 +1,11 @@
+"""
+  Task
+  You have a test string S.
+  Your task is to write a regular expression that matches only and exactly strings of form:
+  abc.def.ghi.jkx, where each variable a, b, c, d, e, f, g, h, i, j, k, x can be any single 
+  character except the newline.
+"""
+
 import re
 import sys
 
