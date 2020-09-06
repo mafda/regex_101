5aa205c9a3c4141c801718bdde08a3bdd2068801
fernanda rodríguez <mafda@users.noreply.github.com>
2020-09-06 17:42:35 -0300

    Create 09_matching_x_repetitions.py

---
+++ example/09_matching_x_repetitions.py
@@ -0,0 +1,16 @@
+"""
+Task
+
+You have a test string S.
+Your task is to write a regex that will match S using the following conditions:
+
+S must be of length equal to 45.
+The first 40 characters should consist of letters(both lowercase and uppercase), or of even digits.
+The last 5 characters should consist of odd digits or whitespace characters.
+"""
+
+import re
+
+Regex_Pattern = r'^[a-zA-Z02468]{40}[13579\s]{5}$'
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
